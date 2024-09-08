fa41fdb05f5bc49f35787f1bb859b18e0a305ead
Syed Muhammad Hamza Naqvi <hamza.naqvi@khired.com>
2024-09-08 09:03:21 +0500

    Added Vet list

--- pet-clinic-web/src/main/java/org/example/sfgpetclinic/petclinicweb/Controller/VetController.java
+++ pet-clinic-web/src/main/java/org/example/sfgpetclinic/petclinicweb/Controller/VetController.java
@@ -1,13 +1,23 @@
 package org.example.sfgpetclinic.petclinicweb.Controller;
 
+import org.example.sfgpetclinic.petclinicdata.Service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping({"/vets","/vets/index"})
-    public String ListVets(){
+    public String ListVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
