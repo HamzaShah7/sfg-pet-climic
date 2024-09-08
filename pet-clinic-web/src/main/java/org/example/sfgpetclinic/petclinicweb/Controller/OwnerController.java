e9f4c027559c615138d63723f1510cfead483522
Syed Muhammad Hamza Naqvi <hamza.naqvi@khired.com>
2024-09-08 08:54:50 +0500

    Added owner List

--- pet-clinic-web/src/main/java/org/example/sfgpetclinic/petclinicweb/Controller/OwnerController.java
+++ pet-clinic-web/src/main/java/org/example/sfgpetclinic/petclinicweb/Controller/OwnerController.java
@@ -1,6 +1,8 @@
 package org.example.sfgpetclinic.petclinicweb.Controller;
 
+import org.example.sfgpetclinic.petclinicdata.Service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping({"","/index"})
-    public String showOwner() {
+    public String showOwner(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index.html";
     }
 }
