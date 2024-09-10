3c53984d155e5c2197892dd55824ee4a30934023
Syed Muhammad Hamza Naqvi <hamza.naqvi@khired.com>
2024-09-10 08:25:05 +0500

    Auto generate ID

--- pet-clinic-data/src/main/java/org/example/sfgpetclinic/petclinicdata/Service/Map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/org/example/sfgpetclinic/petclinicdata/Service/Map/AbstractMapService.java
@@ -1,32 +1,46 @@
 package org.example.sfgpetclinic.petclinicdata.Service.Map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import org.example.sfgpetclinic.petclinicdata.model.BaseEntity;
 
-public abstract class AbstractMapService<T,ID> {
-    protected Map<ID,T> map = new HashMap<>();
+import java.util.*;
 
-    Set<T> findALl(){
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
+
+    Set<T> findALl() {
         return new HashSet<>(map.values());
     }
 
-    T findById(ID id){
+    T findById(ID id) {
         return map.get(id);
     }
 
-    T save(ID id , T object){
-        map.put(id, object);
+    T save(T object) {
+
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        }else {
+            throw new IllegalArgumentException("Object id cannot be null");
+        }
         return object;
     }
 
-    void deleteById(ID id){
+    void deleteById(ID id) {
         map.remove(id);
     }
 
-    void delete(T object){
+    void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        if (map.isEmpty()) {
+            return 1L;
+        }
+        return Collections.max(map.keySet()) + 1;
     }
 
 }
--- pet-clinic-web/src/main/java/org/example/sfgpetclinic/petclinicweb/Bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/org/example/sfgpetclinic/petclinicweb/Bootstrap/DataLoader.java
@@ -23,15 +23,18 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner = new Owner();
-        owner.setId(1L);
         owner.setFirstName("John");
         owner.setLastName("Doe");
         ownerService.save(owner);
 
+        Owner owner1 = new Owner();
+        owner1.setFirstName("Jane");
+        owner1.setLastName("Doe");
+        ownerService.save(owner1);
+
         System.out.println("Loaded Owner...");
 
         Vet vet = new Vet();
-        vet.setId(1L);
         vet.setFirstName("Jane");
         vet.setLastName("Doe");
         vetService.save(vet);
